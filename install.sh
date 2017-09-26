094c95f3ca29c1d98639d87f2d366eb830554329
Benjamin Porter <FreedomBen@users.noreply.github.com>
2017-09-26 17:29:13 -0600

    Add install.sh script

--- install.sh
+++ install.sh
@@ -1,3 +1,81 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# Download latest release for system
+<% require_relative 'erb_helpers' %>
+
+die ()
+{
+    echo "[die]: $1"
+    exit 1
+}
+
+runningOSX ()
+{
+    uname -a | grep "Darwin" > /dev/null
+}
+
+runningLinux ()
+{
+    uname -a | grep -i "Linux" > /dev/null
+}
+
+running32Bit ()
+{
+    [ "$(lshw -c cpu 2>/dev/null | grep width | awk '{print $2}')" = "32" ]
+}
+
+running64Bit ()
+{
+    [ "$(lshw -c cpu 2>/dev/null | grep width | awk '{print $2}')" = "64" ]
+}
+
+runningAmd64 ()
+{
+    [ "$(uname -m)" = 'x86_64' ]
+}
+
+running386 ()
+{
+    local arch="$(uname -m)"
+    [ "$arch" = 'i686' ] || [ "$arch" = 'i386' ]
+}
+
+#runningArm ()
+#{
+#}
+#
+#runningArm64 ()
+#{
+#}
+
+mac_link ()
+{
+    <%= ErbHelpers.url('current_version', 'darwin', 'amd64') %>
+}
+
+linux_link ()
+{
+    <%= ErbHelpers.url('current_version', 'linux', 'amd64') %>
+}
+
+downlink_link ()
+{
+    curl -o "${1}/findref" "${2}"
+}
+
+main ()
+{
+    DEST_DIR="$HOME/bin"
+    [ -n "$1" ] && DEST_DIR="$1"
+
+    if runningLinux; then
+        downlink_link "${DEST_DIR} $(linux_link)"
+    elif runningOSX; then
+        downlink_link "${DEST_DIR} $(mac_link)"
+    else
+        die "Unsupported platform!"
+    fi
+
+    chmod +x "$DEST_DIR/findref"
+}
+
+main $@
