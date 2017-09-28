ab4e630e397fa66684fb3e10f2ea4fe94a210338
Benjamin Porter <FreedomBen@users.noreply.github.com>
2017-09-27 21:49:37 -0600

    Add better output to install script

--- helpers.rb
+++ helpers.rb
@@ -1,4 +1,59 @@
 module Helpers
+  OSES_ARCHES = {
+    'linux' => %w(amd64 386 arm arm64),
+    'windows' => %w(amd64 386),
+    'darwin' => %w(amd64 386)
+  }.freeze
+
+  def self.terminal_color_restore
+    '\033[0m'
+  end
+
+  def self.terminal_colors
+    {
+      red: '\033[0;31m',
+      blue: '\033[0;34m',
+      cyan: '\033[0;36m',
+      green: '\033[0;32m',
+      brown: '\033[0;33m',
+      black: '\033[0;30m',
+      white: '\033[1;37m',
+      purple: '\033[0;35m',
+      yellow: '\033[1;33m',
+      light_red: '\033[1;31m',
+      dark_gray: '\033[1;30m',
+      light_gray: '\033[0;37m',
+      light_blue: '\033[1;34m',
+      light_cyan: '\033[1;36m',
+      light_green: '\033[1;32m',
+      light_purple: '\033[1;35m'
+    }
+  end
+
+  def self.bash_color_restore_line
+    "declare -r color_restore='#{terminal_color_restore}'"
+  end
+
+  def self.bash_color_func(color, code, include_restore: true)
+    <<-EOS.split("\n").map{|l| l.gsub(/\s{6}/, '')}.join("\n").concat("\n")
+      #{include_restore ? bash_color_restore_line : ''}
+      declare -r color_#{color}='#{code}'
+
+      #{color} ()
+      {
+          echo -e "${color_#{color}}${1}${color_restore}\\n"
+      }
+    EOS
+  end
+
+  def self.bash_color_funcs
+    retval = [bash_color_restore_line]
+    terminal_colors.each do |color, code|
+      retval.push(bash_color_func(color, code, include_restore: false))
+    end
+    retval.join
+  end
+
   def self.glob(dir)
     Dir.glob(dir)
       .map{ |d| File.basename(d) }
--- install.sh
+++ install.sh
@@ -1,10 +1,123 @@
 #!/usr/bin/env bash
 
 
+declare -r color_restore='\033[0m'
+declare -r color_red='\033[0;31m'
+
+red ()
+{
+    echo -e "${color_red}${1}${color_restore}\n"
+}
+
+declare -r color_blue='\033[0;34m'
+
+blue ()
+{
+    echo -e "${color_blue}${1}${color_restore}\n"
+}
+
+declare -r color_cyan='\033[0;36m'
+
+cyan ()
+{
+    echo -e "${color_cyan}${1}${color_restore}\n"
+}
+
+declare -r color_green='\033[0;32m'
+
+green ()
+{
+    echo -e "${color_green}${1}${color_restore}\n"
+}
+
+declare -r color_brown='\033[0;33m'
+
+brown ()
+{
+    echo -e "${color_brown}${1}${color_restore}\n"
+}
+
+declare -r color_black='\033[0;30m'
+
+black ()
+{
+    echo -e "${color_black}${1}${color_restore}\n"
+}
+
+declare -r color_white='\033[1;37m'
+
+white ()
+{
+    echo -e "${color_white}${1}${color_restore}\n"
+}
+
+declare -r color_purple='\033[0;35m'
+
+purple ()
+{
+    echo -e "${color_purple}${1}${color_restore}\n"
+}
+
+declare -r color_yellow='\033[1;33m'
+
+yellow ()
+{
+    echo -e "${color_yellow}${1}${color_restore}\n"
+}
+
+declare -r color_light_red='\033[1;31m'
+
+light_red ()
+{
+    echo -e "${color_light_red}${1}${color_restore}\n"
+}
+
+declare -r color_dark_gray='\033[1;30m'
+
+dark_gray ()
+{
+    echo -e "${color_dark_gray}${1}${color_restore}\n"
+}
+
+declare -r color_light_gray='\033[0;37m'
+
+light_gray ()
+{
+    echo -e "${color_light_gray}${1}${color_restore}\n"
+}
+
+declare -r color_light_blue='\033[1;34m'
+
+light_blue ()
+{
+    echo -e "${color_light_blue}${1}${color_restore}\n"
+}
+
+declare -r color_light_cyan='\033[1;36m'
+
+light_cyan ()
+{
+    echo -e "${color_light_cyan}${1}${color_restore}\n"
+}
+
+declare -r color_light_green='\033[1;32m'
+
+light_green ()
+{
+    echo -e "${color_light_green}${1}${color_restore}\n"
+}
+
+declare -r color_light_purple='\033[1;35m'
+
+light_purple ()
+{
+    echo -e "${color_light_purple}${1}${color_restore}\n"
+}
+
 
 die ()
 {
-    echo "[die]: $1"
+    echo "${color_red}[die]: ${1}${color_restore}"
     exit 1
 }
 
@@ -66,14 +179,17 @@ main ()
 {
     dest_dir="${HOME}/bin"
     [ -n "$1" ] && dest_dir="$1"
+    cyan "Creating destination directory '${dest_dir}' if it doesn't exist"
     mkdir -p "${dest_dir}"
 
     bin_name=''
 
     if runningLinux; then
+        cyan 'You are running linux!  Downloading findref v0.0.7 for linux...'
         bin_name='findref'
         downlink_link "${dest_dir}" "$(linux_link)"
     elif runningOSX; then
+        cyan 'You are running macOS!  Downloading findref v0.0.7 for macOS...'
         bin_name='findref'
         downlink_link "${dest_dir}" "$(mac_link)"
     else
@@ -83,10 +199,15 @@ main ()
     cd "${dest_dir}"
 
     # If there's already a findref version, remove it
+    cyan 'Cleaning out any old versions'
     rm -f "${bin_name}"
+    cyan 'Unzipping v0.0.7'
     unzip 'findref.zip'
+    cyan 'Cleaning up the zip file for v0.0.7'
     rm -f 'findref.zip'
+    cyan 'Making ${bin_name} v0.0.7 executable'
     chmod +x "${bin_name}"
+    cyan "All done!  If you can't run findref now, make sure that '${dest_dir}' is in your PATH"
 }
 
 main $@
